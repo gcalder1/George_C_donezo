896b7e3627928124e34ef15268cf1b11da5a571d
unknown <george.calderon2000@gmail.com>
2025-07-03 22:16:18 -0400

    classwork session with jose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
+import todoRouter from "./routes/todo.js";
+import verifyToken from "./middleware/auth.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+//client -> middleware -> main logic -> response
+app.use("/todos", verifyToken, todoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
